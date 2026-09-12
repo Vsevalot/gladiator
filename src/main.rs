d1a86e306c945505d222e582c33e0f8dde366d55
Konstantin Antonov <tino.ant.s@gmail.com>
2026-09-12 15:42:16 +0200

    collision + zombie movement

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use macroquad::prelude::*;
 
 const RADIUS: f32 = 30.0;
 
+#[derive(Debug, Clone, Copy)]
 struct Entity {
     position: Vec2,
     speed: Vec2,
@@ -18,17 +19,36 @@ impl Entity {
     };
     pub const ZOMBIE: Self = Self {
         position: Vec2 { x: 300.0, y: 100.0 },
-        speed: Vec2 { x: 1.0, y: 0.0 },
+        speed: Vec2 { x: -1.0, y: 0.0 },
         radius: RADIUS,
         color: GREEN,
     };
+    pub fn make_zombie(pos: Vec2) -> Self {
+        let mut z = Self::ZOMBIE;
+        z.position = pos;
+        return z;
+    }
+
+    pub fn distance_to(&self, other: &Entity) -> f32 {
+        return (
+            (self.position.x - other.position.x) * (self.position.x - other.position.x) + 
+            (self.position.y - other.position.y) * (self.position.y - other.position.y)
+        ).sqrt()
+    }
+
+    pub fn intersects_with(&self, other: &Entity) -> bool {
+        let out = self.distance_to(other) < self.radius + other.radius;
+        return out;
+    }
 }
 
+#[derive(Debug)]
 struct Field {
     width: f32,
     height: f32,
 }
 
+#[derive(Debug)]
 struct Engine {
     gladiator: Entity,
     zombies: Vec<Entity>,
@@ -36,25 +56,66 @@ struct Engine {
 }
 
 impl Engine {
-    pub fn tick(&mut self) {
-        self.gladiator.position += self.gladiator.speed;
-        if self.gladiator.position.x + self.gladiator.radius > self.field.width {
-            self.gladiator.position.x = self.field.width - self.gladiator.radius;
-        }
-        if self.gladiator.position.x - self.gladiator.radius < 0.0 {
-            self.gladiator.position.x = self.gladiator.radius;
-        }
-        if self.gladiator.position.y + self.gladiator.radius > self.field.height {
-            self.gladiator.position.y = self.field.height - self.gladiator.radius;
-        }
-        if self.gladiator.position.y - self.gladiator.radius < 0.0 {
-            self.gladiator.position.y = self.gladiator.radius;
-        }
-        self.gladiator.speed = Vec2::ZERO;
+    fn get_position_within_field(entity: &Entity, field: &Field) -> Vec2 {
+        let mut new_pos = entity.position;
 
-        for zombie in &mut self.zombies {
-            zombie.position += zombie.speed;
+        if new_pos.x + entity.radius > field.width {
+            new_pos.x = field.width - entity.radius;
         }
+        if new_pos.x - entity.radius < 0.0 {
+            new_pos.x = entity.radius;
+        }
+        if new_pos.y + entity.radius > field.height {
+            new_pos.y = field.height - entity.radius;
+        }
+        if new_pos.y - entity.radius < 0.0 {
+            new_pos.y = entity.radius;
+        }
+
+        return new_pos;
+    }
+
+    fn get_next_zombie_speed_vec(zombie: &Entity, gladiator: &Entity) -> Vec2 {
+        let new_speed = gladiator.position - zombie.position;
+        return 0.2*(new_speed / new_speed.length());
+    }
+    pub fn move_gladiator(&mut self) {
+        let old_pos = self.gladiator.position;
+        self.gladiator.position += self.gladiator.speed;
+        self.gladiator.position = Engine::get_position_within_field(&self.gladiator, &self.field);
+
+        if self.zombies
+           .iter()
+           .any(|z| self.gladiator.intersects_with(&z))
+        {
+            self.gladiator.position = old_pos;
+        }
+
+        self.gladiator.speed = Vec2::ZERO;
+    }
+
+    pub fn move_zombies(&mut self) {
+        let zombies_copy = self.zombies.clone();
+
+        for (i, zombie) in &mut self.zombies.iter_mut().enumerate() {
+            let old_pos = zombie.position;
+            zombie.position += Engine::get_next_zombie_speed_vec(zombie, &self.gladiator);
+            zombie.position = Engine::get_position_within_field(zombie, &self.field);
+
+            if zombies_copy
+               .iter()
+               .enumerate()
+               .any(|(k, z)| { (k != i) && zombie.intersects_with(&z)})
+               || self.gladiator.intersects_with(zombie)
+            {
+                zombie.position = old_pos;
+            }
+        }
+    }
+
+    pub fn tick(&mut self) {
+        self.move_gladiator();
+        self.move_zombies();
     }
 }
 
@@ -66,16 +127,20 @@ async fn main() {
     };
     let mut engine = Engine {
         gladiator: Entity::GLADIATOR,
-        zombies: vec![Entity::ZOMBIE],
+        zombies: vec![
+            Entity::make_zombie(Vec2{x: 200.0, y: 100.0}),
+            Entity::make_zombie(Vec2{x: 350.0, y: 150.0}),
+            Entity::make_zombie(Vec2{x: 500.0, y: 300.0}),
+        ],
         field: field,
     };
 
     loop {
         if is_key_down(KeyCode::W) {
-            engine.gladiator.speed.y -= 1.0;
+            engine.gladiator.speed.y += 1.0;
         }
         if is_key_down(KeyCode::S) {
-            engine.gladiator.speed.y += 1.0;
+            engine.gladiator.speed.y -= 1.0;
         }
         if is_key_down(KeyCode::A) {
             engine.gladiator.speed.x -= 1.0;
@@ -92,7 +157,7 @@ async fn main() {
         clear_background(WHITE);
         draw_circle(
             engine.gladiator.position.x,
-            engine.gladiator.position.y,
+            engine.field.height-engine.gladiator.position.y,
             engine.gladiator.radius,
             engine.gladiator.color,
         );
@@ -100,7 +165,7 @@ async fn main() {
         for zombie in &engine.zombies {
             draw_circle(
                 zombie.position.x,
-                zombie.position.y,
+                engine.field.height-zombie.position.y,
                 zombie.radius,
                 zombie.color,
             );
